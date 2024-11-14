33286f4fc4d685cfcbce8564cac1f22803771843
edogola4 <edogola4@gmail.com>
2024-11-14 21:11:23 +0300

    RecipeAction.js

--- recipe-finder/src/Redux/RecipeReducer.js
+++ recipe-finder/src/Redux/RecipeReducer.js
@@ -4,62 +4,66 @@ import {
     REMOVE_FROM_FAVOURITE,
     SET_RECIPE_DATA,
     SET_RECIPE_ITEM,
-    SET_SEARCH_ITEM
-} from "./RecipeTypes";
-
-const initialState = {
+    SET_SEARCH_ITEM,
+  } from "./RecipeTypes";
+  
+  const initialState = {
     allRecipeData: [],
-    favouriteRecipe: JSON.parse(localStorage.getItem('favouriteRecipe')) || [],
+    favouriteRecipe: JSON.parse(localStorage.getItem("favouriteRecipe")) || [],
     loading: false,
-    recipeItem: {},
     recipeInstruction: [],
-    favouriteList: [],
-    searchItem: ''
-};
-
-const RecipeReducer = (state = initialState, action) => {
+    searchItem: "",
+  };
+  
+  const RecipeReducer = (state = initialState, action) => {
     switch (action.type) {
-        case SEND_API_REQUEST:
-            return {
-                ...state,
-                loading: true
-            };
-        case SET_RECIPE_DATA:
-            return {
-                ...state,
-                allRecipeData: action.payload,
-                loading: false
-            };
-        case SET_RECIPE_ITEM:
-            return {
-                ...state,
-                recipeItem: action.payload,
-                loading: false
-            };
-        case FETCH_RECIPE_INSTRUCTION:
-            return {
-                ...state,
-                recipeInstruction: action.payload,
-                loading: false
-            };
-        case ADD_TO_FAVOURITE:
-            return {
-                ...state,
-                favouriteList: [...state.favouriteList, action.payload]
-            };
-        case REMOVE_FROM_FAVOURITE:
-            return {
-                ...state,
-                favouriteList: state.favouriteList.filter((item) => item.id !== action.payload)
-            };
-        case SET_SEARCH_ITEM:
-            return {
-                ...state,
-                searchItem: action.payload
-            };
-        default:
-            return state;
+      case SEND_API_REQUEST:
+        return {
+          ...state,
+          loading: true,
+        };
+      case SET_RECIPE_DATA:
+        return {
+          ...state,
+          loading: false,
+          allRecipeData: action.payload,
+        };
+      case SET_RECIPE_ITEM:
+        return {
+          ...state,
+          recipeInstruction: action.payload,
+        };
+      case REMOVE_FROM_FAVOURITE:
+        const updatedFavouriteRemove = state.favouriteRecipe.filter(
+          (value) => !(value.id === action.payload)
+        );
+        localStorage.setItem(
+          "favouriteRecipe",
+          JSON.stringify(updatedFavouriteRemove)
+        );
+        return {
+          ...state,
+          favouriteRecipe: updatedFavouriteRemove,
+        };
+      case ADD_TO_FAVOURITE:
+        const updatedFavouriteAdd = [...state.favouriteRecipe, action.payload];
+        localStorage.setItem(
+          "favouriteRecipe",
+          JSON.stringify(updatedFavouriteAdd)
+        );
+        return {
+          ...state,
+          favouriteRecipe: updatedFavouriteAdd,
+        };
+      case SET_SEARCH_ITEM:
+        return {
+          ...state,
+          searchItem: action.payload,
+        };
+  
+      default:
+        return state;
     }
-};
-
-export { RecipeReducer, initialState };
+  };
+  
+  export { RecipeReducer };
